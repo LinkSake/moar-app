a4ad0c0f53f309a203a5f70a8b6c0debb4577666
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-15 16:10:48 -0600

    🎨 Added head/title to main pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-export default function Home() {
+import Head from 'next/head'
+ 
+const Home = () => {
   return (
-    <h1>Welcome to Moar!</h1>
+    <>
+      <Head>
+        <title>Moar! - Sample time tracker</title>
+      </Head>
+      <h1>Welcome to Moar!</h1>
+    </>
   )
 }
+
+export default Home
