695cd2a582b27e871273c573042c96e1dac8c027
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-16 14:33:49 -0600

    🚧 Added empty state for tasks when there's no project

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -1,12 +1,53 @@
 import Head from 'next/head'
+import Note from '../../components/note'
+import { Grid, Header } from 'semantic-ui-react'
  
 const Tasks = () => {
+
+  const head = () => (
+    <Head>
+      <title>Moar! - Tasks</title>
+    </Head>
+  )
+
+  const isMoarEmpty = (projects, tasks) => {
+    if (projects) {
+      return (
+        <Note
+        color='purple'
+        button='Create a project'
+        onClick={() => { alert('Create project modal') }}
+        title="😱 You haven't created a project yet!"
+        message="
+          You can't track your task without a project; 
+          after creating a project here will appear the 
+          task you're currently working on.
+        "/>
+      )
+    } else if (tasks) {
+      // TODO: Otra función que cheque cada projecto para ver si tiene tareas
+    } else {
+      return null
+    }
+  }
+
   return (
     <>
-      <Head>
-        <title>Moar! - Tasks</title>
-      </Head>
-      <h1>Tasks</h1>
+      { head() }
+      <Grid> 
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Header color='purple' as='h1'>
+              Tasks
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            { isMoarEmpty(true, true) }
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </>
   )
 }
