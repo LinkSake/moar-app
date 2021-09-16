38dc22a2b72137b7a70f53cf5dc46a01ab7142d6
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-16 14:23:15 -0600

    ✨ Added empty state to projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,12 +1,52 @@
 import Head from 'next/head'
+import Note from '../../components/note'
+import { Grid, Header } from 'semantic-ui-react'
  
 const Projects = () => {
+
+  const head = () => (
+    <Head>
+      <title>Moar! - Projects</title>
+    </Head>
+  )
+
+  const isProjectEmpty = project => {
+    if (project) {
+      return (
+        <Note
+        color='purple'
+        button='Create a project'
+        onClick={() => { alert('Create project modal') }}
+        title="😱 You haven't created a project yet!"
+        message="
+          You can't track your task without a project; 
+          after creating a project here will appear the 
+          task you're currently working on.
+        "/>
+      )
+    } else {
+      return null
+    }
+  }
+
+
   return (
     <>
-      <Head>
-        <title>Moar! - Projects</title>
-      </Head>
-      <h1>Projects</h1>
+      { head() }
+      <Grid> 
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Header color='purple' as='h1'>
+              Projects
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            { isProjectEmpty(true) }
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </>
   )
 }
