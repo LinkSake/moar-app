f128d2bcab52fe5ef9650328ff3574d0a183ad67
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-16 12:39:06 -0600

    🚧 Added logo/title to landing

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
 import Head from 'next/head'
+import Styles from '../styles/home.module.css'
+import { Grid, Header, Icon } from 'semantic-ui-react'
  
 const Home = () => {
+
+  const head = () => (
+    <Head>
+      <title>Moar! - Sample time tracker</title>
+    </Head>
+  )
+
+  const logo = () => (
+    <div className={Styles.container}>
+      <Header 
+      icon
+      as='h1'
+      color='purple'
+      >
+        <Icon name='stopwatch'/>
+        <i className={Styles.title}> 
+          Moar! 
+        </i>
+      </Header>
+      <Header.Subheader className={Styles.subheader} >
+        A sample time tracker
+      </Header.Subheader>
+    </div>
+  )
+
   return (
     <>
-      <Head>
-        <title>Moar! - Sample time tracker</title>
-      </Head>
-      <h1>Welcome to Moar!</h1>
+      { head() }  
+      <Grid>
+        <Grid.Row>
+          <Grid.Column textAlign='center' width={16}>
+            { logo() }
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
     </>
   )
 }
