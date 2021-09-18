5882272f7e5e49581aa05c724fae22b034970774
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-17 18:36:00 -0600

    ✨ Added empty state to task page

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -10,24 +10,60 @@ const Tasks = () => {
     </Head>
   )
 
-  const isMoarEmpty = (projects, tasks) => {
-    if (projects) {
+  const getProjects = (projects) => {
+    if (projects.length === 0) {
       return (
         <Note
         color='purple'
         button='Create a project'
-        onClick={() => { alert('Create project modal') }}
         title="😱 You haven't created a project yet!"
+        onClick={() => { alert('Create project modal') }}
         message="
           You can't track your task without a project; 
           after creating a project here will appear the 
           task you're currently working on.
         "/>
       )
-    } else if (tasks) {
-      // TODO: Otra función que cheque cada projecto para ver si tiene tareas
+    } else { 
+      return projects.map(project => {
+        return getTasksComponents(project)
+      })
+    }
+  }
+
+  const getTasksComponents = (project) => {
+    if (project.items.length === 0) {
+      return (
+        <div key={project.name}>
+          <Header as='h2'>
+            {project.name}
+          </Header>
+            <Note
+            color='purple'
+            button='Create a task'
+            title="😅 There aren't tasks on this project."
+            onClick={() => { alert('Create task modal') }}
+            message="
+            To make use of Moar and start tracking time,
+            create a task!"
+            />
+        </div>
+      )
     } else {
-      return null
+      let tasks = project.items.map(task => {
+        //  Create a task component
+        return (
+          <li key={task.name}>
+            { task.name }
+          </li>
+        )
+      })
+      tasks.unshift(
+        <Header key={project.name} as='h2'>
+          {project.name}
+        </Header>
+      )
+      return tasks
     }
   }
 
@@ -44,7 +80,7 @@ const Tasks = () => {
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width={16}>
-            { isMoarEmpty(true, true) }
+            { getProjects([]) }
           </Grid.Column>
         </Grid.Row>
       </Grid>
