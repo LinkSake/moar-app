16a8c3bcc1bb140cb49c1b55d2b99217d02e99c5
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-18 14:28:43 -0600

    ✨ Added task list/table

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { useContext } from 'react'
 import { Context } from '../../context'
 import Note from '../../components/note'
-import { Grid, Header } from 'semantic-ui-react'
+import { Button, Grid, Header, Table } from 'semantic-ui-react'
  
 const Tasks = () => {
 
@@ -58,20 +58,72 @@ const Tasks = () => {
         </div>
       )
     } else {
-      let tasks = project.items.map(task => {
-        //  Create a task component
-        return (
-          <li key={task.name}>
-            { task.name }
-          </li>
-        )
-      })
-      tasks.unshift(
-        <Header key={project.name} as='h2'>
-          {project.name}
-        </Header>
+      return (
+        <>
+          <Header key={project.name} as='h2'>
+            { project.name }
+          </Header>
+          <Table basic unstackable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Task name</Table.HeaderCell>
+                <Table.HeaderCell>Time elapsed</Table.HeaderCell>
+                <Table.HeaderCell/>
+                <Table.HeaderCell/>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {project.items.map(task => (
+                <Table.Row key={task.name}>
+                  <Table.Cell>{task.name}</Table.Cell>
+                  <Table.Cell>
+                    { task.running ? (
+                      <i>Currently working on this task...</i>
+                    ) : task.elapsed }
+                  </Table.Cell>
+                  <Table.Cell collapsing>
+                    <Button
+                    color='purple'
+                    disabled={task.running ? true : false}
+                    onClick={() => { alert('Edit task modal') }}
+                    >
+                      Edit/Delete
+                    </Button>
+                  </Table.Cell>
+                  <Table.Cell collapsing>
+                    { task.running ? (
+                      <Button
+                      basic
+                      color='red'
+                      onClick={() => { alert('Remove task modal') }}
+                      >
+                        Stop
+                      </Button>
+                    ) : (
+                      <Button
+                      basic
+                      color='purple'
+                      onClick={() => { alert('Remove task modal') }}
+                      >
+                        Start
+                      </Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+          <Button 
+          basic 
+          color='purple' 
+          floated='right'
+          content='+ Add task' 
+          onClick={() => { alert('New task') }}
+          />
+          <br/>
+          <br/>
+        </>
       )
-      return tasks
     }
   }
 
